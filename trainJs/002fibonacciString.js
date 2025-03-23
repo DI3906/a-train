da33a29c21b460517af270624c797bfee99ca006
Diego Garcia <diegg066.99@gmail.com>
2025-03-23 14:07:28 -0600

    Fibonacci recursion

---
+++ trainJs/002fibonacciString.js
@@ -0,0 +1,15 @@
+/* 
+A Fibonacci string is a precedence of the Fibonacci series. 
+It works with any two characters of the English alphabet (as opposed to 
+the numbers 0 and 1 in the Fibonacci series) as the initial items and 
+concatenates them together as it progresses similarly to the Fibonacci series.
+
+Examples
+fibStr(3, ["j", "h"]) ➞ "j, h, hj"
+fibStr(5, ["e", "a"]) ➞ "e, a, ae, aea, aeaae"
+fibStr(6, ["n", "k"]) ➞ "n, k, kn, knk, knkkn, knkknknk"
+*/
+
+function fibStr(n, str) {
+	
+}
